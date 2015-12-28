f4d2001c9873422662bb24d3e10c583e9c7eaa8f
Carlos Calderon <cdcalderon@gmail.com>
2015-12-28 05:27:09 -0600

    add addNote deleteNote features with NSFetchedResultsController

---
+++ Papers/AddNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddNoteViewController.h
+//  Papers
+//
+//  Created by Lorena Calderon on 12/28/15.
+//  Copyright © 2015 contructysystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNoteViewController : UIViewController
+
+@end
